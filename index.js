e52fdb90b5f6bd412fb79ba6cb50b05a29350fe3
Harri Kovalainen <hakovala@gmail.com>
2015-02-24 19:38:53 +0200

    Stupid colors
    Signed-off-by: Harri Kovalainen <hakovala@gmail.com>

--- index.js
+++ index.js
@@ -1,53 +1,63 @@
 "use strict";
 
-var Colors = require('./colors');
-
 var names = ['black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white'];
 var namesBright = names.map(function(name) { return name + 'Bright'; });
+var namesStyle = ['bold', 'dim', 'underline', 'blink', null, 'invert', 'hidden'];
 
-function getColorIndex(color) {
-	var index = color;
-	if (typeof color === 'string') {
-		index = names.indexOf(color);
-		if (index != -1) return index;
-		index = namesBright.indexOf(color);
-	}
-	return index;
+var RESET = '\x1b[0m';
+
+var Colors = {
+	fg: {},
+	bg: {},
+	style: {},
+};
+
+// generate foreground normal colors
+for (var i in names) {
+	Colors.fg[names[i]] = +i + 30;
+}
+// generate foreground bright colors
+for (var i in namesBright) {
+	Colors.fg[namesBright[i]] = +i + 90;
+}
+
+// generate background normal colors
+for (var i in names) {
+	Colors.bg[names[i]] = +i + 40;
+}
+// generate background bright colors
+for (var i in namesBright) {
+	Colors.bg[namesBright[i]] = +i + 100;
+}
+
+// generate style attributes
+for (var i in namesStyle) {
+	if (!namesStyle[i])
+		continue;
+	Colors.style[namesStyle[i]] = +i + 1;
+}
+
+function formatColor(color) {
+	color = color || {};
+	var fg = Colors.fg[color.fg] || 39;
+	var bg = Colors.bg[color.bg] || 49;
+	var style = Colors.style[color.style] || 0;
+
+//	var code = '\x1b';
+
+	return '\x1b[' + style + ';' + fg + ';' + bg + 'm';
 }
 
 function colorize(str, colors) {
-	if (typeof colors !== 'object')
+	if (!str || typeof colors !== 'object')
 		return str;
 
 	if (typeof str !== 'string')
 		throw new Error('Only strings will blend');
 
-	var color;
-	if (colors.fg) {
-		if (typeof colors.fg === 'string') {
-			color = Colors.standard.fg[names.indexOf(colors.fg)];
-			color = color || Colors.bright.fg[namesBright.indexOf(colors.fg)];
-		} else if (typeof colors.fg === 'number') {
-			color = Colors.colors[colors.fg];
-		}
-		if (color) {
-			str = color + str + Colors.reset;
-		}
-	}
-	if (colors.bg) {
-		if (typeof colors.bg === 'string') {
-			color = Colors.standard.bg[names.indexOf(colors.bg)];
-			color = color || Colors.bright.bg[namesBright.indexOf(colors.bg)];
-		} else if (typeof colors.bg === 'number') {
-			color = Colors.colors[colors.bg];
-		}
-		if (color) {
-			str = color + str + Colors.reset;
-		}
-	}
-	return str;
+	return formatColor(colors) + str + RESET;
 }
 module.exports = colorize;
 module.exports.colors = Colors;
-module.exports.normal = names;
-module.exports.bright = namesBright;
+module.exports.names = names.concat(namesBright);
+module.exports.styles = namesStyle.filter(function (name) { return !!name; });
